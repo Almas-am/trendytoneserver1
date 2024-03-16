9c9ce872546dec570a2ace282060d8393d4188ef
Almas-am <amrishatiwari408@gmail.com>
2024-03-16 15:50:54 +0530

    dependencies n logo

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ connectToMongo();
 let cors= require("cors");
 
 const app=express();
+
+const path = require('path');
+
+const shortid = require('shortid');
+
+const razorpay = require('razorpay');
+
 const port=process.env.PORT || 8000;
+
+//initialize razorpay credentials
+
+const razorpay =  new razorpay({
+    key_id: "rzp_test_ATIDqpJJTUeTEw",
+    key_secret:"Atf2j4yxFH1kmNqenFwGwp2w"
+
+})
 
 app.use(express.json())
 app.use(cors());
